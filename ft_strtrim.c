f90640c5db5750d23fae7281f123f3a852d1e8d0
Eonjoo-Oh <eonjoo@Ejayui-MacBookAir.local>
2022-11-24 00:03:53 +0900

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -13,77 +13,25 @@
 #include "libft.h"
 #include <stdlib.h>
 
-int	find_start(char const *s1, char const *set)
+char *ft_strtrim(char const *s1, char const *set)
 {
-	int	i;
-	int	j;
-	int	start;
+	size_t start;
+	size_t end;
+	size_t i;
+	char *result;
 
 	i = 0;
-	while (s1[i])
-	{
-		start = i;
-		j = 0;
-		while (set[j])
-		{
-			if (s1[i] == set[j])
-			{
-				i++;
-				break ;
-			}
-			j++;
-		}
-		if (start == i)
-			break ;
-	}
-	return (start);
-}
-
-int	find_end(const char *s1, char const *set, int start)
-{
-	int	len;
-	int	end;
-	int	j;
-
-	len = (int)ft_strlen(s1);
-	while (len > start)
-	{
-		end = len - 1;
-		j = 0;
-		while (set[j])
-		{
-			if (s1[len - 1] == set[j])
-			{
-				len--;
-				break ;
-			}
-			j++;
-		}
-		if (end == len - 1)
-			break ;
-	}
-	return (end);
-}
-
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	int		start;
-	int		end;
-	int		i;
-	char	*result;
-	int		size;
-
-	start = find_start(s1, set);
-	end = find_end(s1, set, start);
-	size = end - start;
-	if ((start == ((int)(ft_strlen(s1)) - 1)) || size == 0)
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (ft_strchr(set, s1[start]) && s1[start])
+		start++;
+	while (ft_strchr(set, s1[end]) && end > 0)
+		end--;
+	if (start == ft_strlen(s1))
 		return (ft_strdup(""));
-	if (end - start < 0)
-		size = -1;
-	result = (char *)malloc(sizeof(char) * (size + 2));
+	result = (char *)malloc(sizeof(char) * (end - start + 2));
 	if (!result)
 		return (0);
-	i = 0;
 	while (start + i <= end)
 	{
 		result[i] = s1[start + i];
