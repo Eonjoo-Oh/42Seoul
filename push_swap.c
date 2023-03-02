634ab8760c2d17497f785826e0a86337ead68567
Eonjoo Oh <eoh@cx2r4s4.42seoul.kr>
2023-03-02 18:30:52 +0900

    push_swap

--- push_swap.c
+++ push_swap.c
@@ -1,14 +1,42 @@
 #include "push_swap.h"
 #include <stdio.h>
-
+/*
 void push_swap(int argc, char **argv)
 {
     t_list *stack_a;
+    
+    if (argc == 1)
+        exit(1);
+    stack_a = first_stack(argv);
+    if (stack_a == 0)
+        exit(1);
+}
+*/
+void push_swap(int argc, char **argv)
+{
+    t_list *stack_a;
+    t_node *node;
 
     if (argc == 1)
         exit(1);
     stack_a = first_stack(argv);
     if (stack_a == 0)
-        write(2, "Error\n", 6);
-    printf("%d\n", stack_a->head->content);
+    {
+        write (2, "Error\n", 6);
+        exit(1);
+    }
+    node = stack_a->head;
+    while (node->next != stack_a->head)
+    {
+        printf("%d\n", node->content);
+        node = node->next;
+    }
+    printf("%d\n", node->content); 
+}
+
+int main(int argc, char ** argv)
+{
+    push_swap(argc, argv);
+    //system ("leaks a.out");
+    return (0);
 }
