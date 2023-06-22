76c5e158cafae560d46f13724fb7ea9f01e90876
Eonjoo Oh <eoh@cx1r2s4.42seoul.kr>
2023-06-22 18:13:14 +0900

    making new bfs

--- map_bfs.c
+++ map_bfs.c
@@ -1,58 +1,87 @@
 #include "so_long.h"
 
-/*typedef	struct s_visit {
-	int	i;
-	int	j;
-}	t_visit;*/
-
-typedef	struct s_bfs {
+typedef struct s_bfs {
 	int	i;
 	int	j;
 	int	visited;
 }	t_bfs;
 
 typedef struct s_queue {
-	int	front;
-	int	rear;
-	t_bfs	*bfs;
+	int				front;
+	int				rear;
+	struct s_bfs	*bfs;
 }	t_queue;
 
-void	enqueue(t_queue *q, int i, int j)
+int	queue_empty(t_queue *queue)
 {
-	if (q->bfs[0]->i == -1)
-	{
+	if (queue->front == queue->rear)
+		return (0);
+	else
+		return (1);
+}//비어있으면 0을, 비어있지 않으면 1을 반환
 
-	}//첫번째요소
+int	queue_full(t_queue *queue, t_map *map)
+{
+	if (queue->rear == map->l * map->w)
+		return (0);
+	else
+		return (1);
+}//꽉 차있으면 0을 꽉 차있지 않으면 1을 반환
+
+void	dequeue(t_queue *queue)
+{
+	if (queue_empty(queue) == 0)
+		return (0);
+	queue->front++;
+}
+void	enqueue(t_queue *queue, t_map *map)
+{
+	if (queue_full(queue, map) == 0)
+		return (0);
+	queue->rear++;
+	queue->bfs[queue->rear].i = map->p_pos[0];
+	queue->bfs[queue->rear].j = map->p_pos[1];
+	queue->bfs[queue->rear].visited = 1;//필요한지 다시 생각해보기. enqueue하는 순간 방문 했다는 의미니까 필요가 없을 수도..?
 }
 
-t_queue	*init_queue(t_map *map)
+void	enqueue_unvisited(t_queue *queue, t_map, map)
 {
-	int		w;
-	int		l;
+
+}
+
+t_queue	*init_queue (t_map *map)
+{
 	t_queue	*queue;
+	t_bfs	*bfs;
 
 	queue = (t_queue *)malloc(sizeof(t_queue));
 	if (!queue)
 		return (0);
-	queue->front = 0;
-	queue->rear = 0;
-	//queue->bfs = 0;
-	//queue->visited = 0;
-	queue->bfs = (t_bfs *)malloc(sizeof(t_bfs) * (w * l));
-	if (!queue->bfs)
-		return (0);
-	queue->bfs[0]->i = -1;
-	queue->bfs[1]->j = -1;
-	queue->bfs->visited = -1;
-
+	bfs = (t_bfs *)malloc(sizeof(t_queue) * (map->l * map->w));
+	if (bfs == 0)
+		return (0);//bfs안에 값을 다 초기화하고 싶은데 어떻게 하지? 그리고 이미 방문한 건 앞에서 조회가능하니 visited는 필요없나?
+	queue->front = -1;
+	queue->rear = -1;
+	queue->bfs = bfs;
 	return (queue);
 }
 
-int	check_bfs(t_map *map)
+int	bfs_main(t_map *map)
 {
 	t_queue	*q;
+	int		x;
+	int		y;//first position이 map->p_pos에 저장되어있지 않나?->이걸 구하는 함수 get_position
 
 	q = init_queue(map);
+	if (!q)
+		error_msg("malloc error");
 	get_position(map);
-	enqueue(q, map->p_pos[0], map->p_pos[1]);
+	enqueue(q, map);
+	while(queue_empty(q) != 0)
+	{
+		enqueue_unvisited(q, map);
+		dequeue(q);
+	}
+	
+
 }
