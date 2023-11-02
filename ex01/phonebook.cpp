637594d837c01cb9ee36ff74bf4f687295b2a9ab
Eonjoo Oh <eoh@cx2r2s4.42seoul.kr>
2023-11-02 18:19:19 +0900

    ex01 하는중

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 class PhoneBook
 {
@@ -7,16 +8,26 @@ class PhoneBook
 
 class Contact
 {
-	public:
-		char	*FirstName;
-		char	*LastName;
-		char	*NickName;
-		int		PhoneNumber;
-		char	*DarkestSecert
+	private:
+		std::string	FirstName;
+		std::string	LastName;
+		std::string	NickName;
+		std::string	PhoneNumber;
+		std::string	DarkestSeceret;
 };
 
 int main()
 {
+	PhoneBook	PhoneBook;
+	char	*command;
 
+	while (1)
+	{
+		std::cout << "What service would you like to use?" << std::endl;
+		std::cout << "1.ADD 2.SEARCH 3.EXIT";
+		std::cin << command;
+		if ()
+	}
 
 }
+//string은 문자열이고, char *은?
