7645ee39fdea4778dae0882379577763858b97c6
Eonjoo Oh <eoh@cx1r3s3.42seoul.kr>
2023-11-14 18:26:50 +0900

    ex01 객체의 배열 초기화 어떻게?

---
+++ ex01/Zombie.hpp
@@ -0,0 +1,15 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+
+class	Zombie(std::string name)
+{
+	private:
+		std::string name;
+	public:
+		Zombie(name);
+		~Zombie();
+}
+
+#endif
