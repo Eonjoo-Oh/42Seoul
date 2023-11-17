8d407fc7ed9c2c4fd48b2567ec74523fab972edf
Eonjoo Oh <eoh@cx2r3s7.42seoul.kr>
2023-11-17 17:39:27 +0900

    ex04 draft

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -22,17 +22,38 @@ int main(int argc, char **argv)
 	}
 
 	std::ifstream	inputFile(argv[1]);
+	std::string		inputFileName = argv[1];
 	std::string		oldWord = argv[2];
 	std::string		newWord = argv[3];
 	std::string		line;
+
 	if (!inputFile.is_open())
 	{
-		std::cout << "The file does not exist" << std::endl;
+		std::cerr << "The file does not exist" << std::endl;
 		return (1);
 	}
+	std::string tempFileName = inputFileName + "_temp.txt";
+	std::ofstream	tempFile(tempFileName);
+	if (!tempFile.is_open())
+	{
+		std::cerr << "The file does not exist" << std::endl;
+		return (1);
+	}
+
 	while (std::getline(inputFile, line))
 	{
-		rewrite(line, oldWord, newWord); 
-		std::cout << line;
+		rewrite(line, oldWord, newWord);
+		tempFile << line << std::endl;
 	}
+	if (std::remove(argv[1]) != 0)
+	{
+		std::cerr << "remove Error" << std::endl;
+		return (1);
+	}
+	if (std::rename(tempFileName.c_str(), argv[1]) != 0)
+	{
+		std::cerr << "rename Error" << std::endl;
+		return (1);
+	}
+	return (0);
 }
