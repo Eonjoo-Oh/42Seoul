ba41cd2524cb2541d58532c4a5277ff65c922fce
Eonjoo Oh <eoh@c4r9s8.42seoul.kr>
2023-11-20 14:45:13 +0900

    ex05 made test as required

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -2,11 +2,15 @@
 
 int main()
 {
-	std::string	level;
-	Harl		harl;
+	Harl	harl;
 
-	std::cout << "Hello Harl, Please enter your Angry Level" << std::endl;
-	std::cout << "debug | info | warning | error" << std::endl;
-	std::cin >> level;
-	harl.complain(level);
+	harl.complain("debug");
+	harl.complain("info");
+	harl.complain("warning");
+	harl.complain("error");
+
+	harl.complain("debug");
+	harl.complain("info");
+	harl.complain("warning");
+	harl.complain("error");
 }
