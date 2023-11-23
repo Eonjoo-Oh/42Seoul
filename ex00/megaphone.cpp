ebce0d72084d0da2dfc20ef681be8dadd28d4096
Eonjoo Oh <eoh@cx2r3s7.42seoul.kr>
2023-11-23 13:50:17 +0900

    ex00 use toupper

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,32 +1,16 @@
 #include <iostream>
 #define DefaultMessage "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
 
-class Megaphone
-{
-	public:
-		char	capitalConverter(int c);
-};
-
-char	Megaphone::capitalConverter(int c)
-{
-	if (c >= 'a' && c <= 'z')
-		return c - 32;
-	else
-		return c;
-}
-
 int main(int argc, char **argv)
 {
-	Megaphone megaphone;
 
 	if (argc == 1)
 		std::cout << DefaultMessage;
-	else
+	for(int i = 1; argv[i] != NULL; i++)
 	{
-		for(int i = 1; i < argc; i++)
+		for(int j = 0; argv[i][j] != '\0'; j++)
 		{
-			for(int j = 0; argv[i][j] != '\0'; j++)
-				std::cout << megaphone.capitalConverter(argv[i][j]);
+			std::cout << static_cast<char>(std::toupper(argv[i][j])) ;
 		}
 	}
 	std::cout << std::endl;
