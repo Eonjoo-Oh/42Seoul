4f85187fd03c9393f23ee6dfb980d4ad2ccd350e
Eonjoo Oh <eoh@cx2r3s6.42seoul.kr>
2023-12-06 14:12:01 +0900

    delete comment and readmefile

--- ex00/randomChump.cpp
+++ ex00/randomChump.cpp
@@ -1,16 +1,7 @@
 #include "Zombie.hpp"
 
-//void	randomChump(std::string name)
-//{
-//	Zombie* zombie;
-
-//	zombie = newZombie(name);
-//	zombie->announce();
-//	delete zombie;
-//}//heap
-//stack에 heap 영역 중 해당 영역에 선언한 이유
 void	randomChump(std::string name)
 {
 	Zombie	zombie(name);
 	zombie.announce();
-}//stack
+}
