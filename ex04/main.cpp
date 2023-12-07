39c3e7b7282cecf11556c3fc34fd70ba7f0f7c6d
Eonjoo Oh <eoh@cx2r3s5.42seoul.kr>
2023-12-07 17:20:55 +0900

    check the exception when same word input and null input

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -4,12 +4,21 @@
 
 int main(int argc, char **argv)
 {
+	std::string	oldWord;
+	std::string	newWord;
+
 	if (argc != 4)
 	{
 		std::cout << "Check the number of argument" << std::endl;
 		return (1);
 	}
-	
+	oldWord = argv[2];
+	newWord = argv[3];
+	if (oldWord == newWord || oldWord == "")
+	{
+		std::cout << "invalid input!" << std::endl;
+		return (1);
+	}
 	Sed	sed(argv[1], argv[2], argv[3]);
 	if (sed.fileProcess() == false)
 		return (1);
