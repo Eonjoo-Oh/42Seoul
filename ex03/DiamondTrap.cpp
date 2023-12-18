fab266ed852a6a87042da661261ec73393b1a316
Eonjoo Oh <eoh@cx2r3s5.42seoul.kr>
2023-12-18 16:18:10 +0900

    add initializer to DiamondTrap copy constructor

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -20,7 +20,7 @@ DiamondTrap::DiamondTrap(std::string name) : FragTrap(), ScavTrap()
 	_attackDamage = FragTrap::getDefaultAttackDamage();
 }
 
-DiamondTrap::DiamondTrap(const DiamondTrap &diamondTrap) : FragTrap(diamondTrap), ScavTrap(diamondTrap)
+DiamondTrap::DiamondTrap(const DiamondTrap &diamondTrap) : ClapTrap(diamondTrap), FragTrap(diamondTrap), ScavTrap(diamondTrap)
 {
 	std::cout << "DiamondTrap Copy Constructor called" << std::endl;
 	*this = diamondTrap;
