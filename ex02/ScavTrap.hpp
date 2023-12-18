bddce27460f80f7f3a035dfa4443d960aa3a0460
Eonjoo Oh <eoh@cx2r3s5.42seoul.kr>
2023-12-18 15:47:12 +0900

    modify scavtrap in ex02

--- ex02/ScavTrap.hpp
+++ ex02/ScavTrap.hpp
@@ -3,17 +3,12 @@
 
 #include "ClapTrap.hpp"
 
-class ScavTrap : public ClapTrap{
+class ScavTrap : public ClapTrap {
 	public:
 		ScavTrap();
-		ScavTrap(std::string name) : ClapTrap(name) {
-			std::cout << "ScavTrap constructor called" << std::endl;
-			_hitPoints = 100;
-			_energyPoints = 50;
-			_attackDamage = 20;
-		}
-		ScavTrap(const ScavTrap &scavTrap);
-		ScavTrap& operator=(const ScavTrap &scavTrap);
+		ScavTrap(std::string name);
+		ScavTrap(const ScavTrap &scavtrap);
+		ScavTrap& operator=(const ScavTrap &scavtrap);
 		~ScavTrap();
 		void	guardGate();
 		void	attack(const std::string &target);
