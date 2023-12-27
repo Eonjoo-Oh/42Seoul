ea070941ee0467b65301eb05da9d0306359965ef
Eonjoo Oh <eoh@cx2r3s7.42seoul.kr>
2023-12-27 18:46:25 +0900

    ex02: solve assignment operator problem:

--- ex02/ShrubberyCreationForm.cpp
+++ ex02/ShrubberyCreationForm.cpp
@@ -7,7 +7,12 @@ ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm &obj) :
 	*this = obj;
 };
 
-//ShrubberyCreationForm &ShrubberyCreationForm::operator=(const ShrubberyCreationForm &obj) {}
+ShrubberyCreationForm &ShrubberyCreationForm::operator=(const ShrubberyCreationForm &obj)
+{
+	if (this != &obj)
+		AForm::operator=(obj);
+	return (*this);
+}
 
 ShrubberyCreationForm::~ShrubberyCreationForm() {};
 
--- ex02/ShrubberyCreationForm.hpp
+++ ex02/ShrubberyCreationForm.hpp
@@ -10,7 +10,7 @@ class	ShrubberyCreationForm : public AForm
 	public :
 		ShrubberyCreationForm();
 		ShrubberyCreationForm(const ShrubberyCreationForm &obj);
-		//ShrubberyCreationForm &operator=(const ShrubberyCreationForm &obj);
+		ShrubberyCreationForm &operator=(const ShrubberyCreationForm &obj);
 		~ShrubberyCreationForm();
 		bool	execute(Bureaucrat const &executor) const;
 };
