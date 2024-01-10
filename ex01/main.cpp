13e695cb43c2705ba112559a9aefd347cc2280a3
Eonjoo Oh <eoh@c6r3s4.42seoul.kr>
2024-01-10 18:33:23 +0900

    ex01: done

---
+++ ex01/main.cpp
@@ -0,0 +1,21 @@
+#include "Serializer.hpp"
+
+int main()
+{
+	Data	data1;
+	Data	*data2 = NULL;
+	uintptr_t	uintptr;
+
+	data1.name = "bokdong";
+	data1.age = 3;
+	std::cout << "data1 : " << &data1 << std::endl;
+	std::cout << "data2 : " << data2 << std::endl;
+	
+	uintptr = Serializer::serialize(&data1);
+	std::cout << "uintptr: " << uintptr << std::endl;
+	
+	data2 = Serializer::deserialize(uintptr);
+	std::cout << "deserialized data2: " << data2 << std::endl;
+	std::cout << "deserialized data2 name: " << data2->name << std::endl;
+	std::cout << "deserialized data2 age: " << data2->age << std::endl;
+}
