2c7dc03a51765cd29d798ca2cda6f7933563bb65
Eonjoo Oh <eoh@cx2r3s7.42seoul.kr>
2024-01-12 11:16:26 +0900

    ex01: change variable name and add new line to clear

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,18 +4,18 @@ int main()
 {
 	Data		data1;
 	Data		*data2 = NULL;
-	uintptr_t	uintptr;
+	uintptr_t	serialNum;
 
 	data1.name = "bokdong";
 	data1.age = 3;
 	std::cout << "data1 : " << &data1 << std::endl;
-	std::cout << "data2 : " << data2 << std::endl;
+	std::cout << "data2 : " << data2 << std::endl << std::endl;
 	
-	uintptr = Serializer::serialize(&data1);
-	std::cout << "serialize data to uintptr: " << uintptr << std::endl;
+	serialNum = Serializer::serialize(&data1);
+	std::cout << "serialize data1 to serialNum: " << serialNum << std::endl << std::endl;
 	
-	data2 = Serializer::deserialize(uintptr);
-	std::cout << "deserialize uintptr to data2: " << data2 << std::endl;
+	data2 = Serializer::deserialize(serialNum);
+	std::cout << "deserialize serialNum to data2: " << data2 << std::endl;
 	std::cout << "data2 name: " << data2->name << std::endl;
 	std::cout << "data2 age: " << data2->age << std::endl;
 }
