e34b7cfe746aaa8c40160c9a293c3a5fa21f007f
Eonjoo-Oh <eonjoo@Ejayui-MacBookAir.local>
2024-01-15 01:02:39 +0900

    ex01: add occf to private

--- ex01/Serializer.hpp
+++ ex01/Serializer.hpp
@@ -7,11 +7,12 @@
 
 class	Serializer
 {
-	public :
+	private :
 		Serializer();
 		Serializer(const Serializer &obj);
 		Serializer &operator=(const Serializer &obj);
 		~Serializer();
+	public :
 		static uintptr_t	serialize(Data *ptr);
 		static Data			*deserialize(uintptr_t raw);
 };
