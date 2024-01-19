0562b7b67203aab54d0396d1dbf061e48ce9c965
Eonjoo-Oh <eonjoo@192.168.nate.com>
2024-01-19 14:47:34 +0900

    ex01: made shortestSpan

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -11,7 +11,7 @@ Span::Span(const Span &obj)
 
 Span &Span::operator=(const Span &obj)
 {
-	v = obj.v;//std::vector의 복사 대입연산자에서 깊은 복사를 수행
+	_v = obj._v;//std::vector의 복사 대입연산자에서 깊은 복사를 수행
 	N = obj.N;
 }
 
@@ -28,12 +28,14 @@ void	Span::addNumber(int n)
 void	Span::addNumbers(std::vector<int> v)
 {
 //벡터와 벡터를 합치는 로직
-}
+}//매개변수를 뭘로 받아야하지?
 
 unsigned int	Span::shortestSpan()
 {
 	if (_v.empty() || _v.size() == 1)
 		throw(std::logic_error("no span can be found!"));
+	sort(_v.begin(), _v.end());
+	return (abs(_v[0] - _v[1]));
 }
 
 unsigned int	Span::longestSpan()
