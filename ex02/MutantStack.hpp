6efb084cdf70ecf592dfb5212078e907d39779d2
Eonjoo Oh <eoh@cx2r3s5.42seoul.kr>
2024-01-23 19:33:48 +0900

    ex02: made MutantStack.hpp: draft / main.cpp: list test

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,37 @@
+# pragma once
+
+#include <stack>
+
+template <typename T>
+class	MutantStack : public std::stack<T>
+{
+	public :
+		MutantStack();
+		MutantStack(const MutantStack &obj);
+		MutantStack &operator=(const MutantStack &obj);
+		~MutantStack();
+
+		typedef typename MutantStack<T>::stack::container_type::iterator;
+
+};
+
+template <typename T>
+MutantStack<T>::MutantStack() {}
+
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack &obj) 
+{
+	*this = obj;
+}
+
+template <typename T>
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack<T> &obj)
+{
+	(void)obj;
+	return (*this);
+}
+
+template <typename T>
+MutantStack<T>::~MutantStack() {}
+
+
